f8d550ddf4e01be73dd58100ec617052e77eee71
Skhoooler <dor15004@byui.edu>
2021-06-03 11:56:01 -0600

    Added some more to player class, and finished initGame()

--- Hunger Games.py
+++ Hunger Games.py
@@ -4,33 +4,51 @@
 import sys
 
 def main():
-    print("test1")
-    playerList = initGame(1)
+    playerList = initGame(5)
 
-    print(playerList)
     pass
 
 def initGame(numOfDistricts=12):
     districtNum = 1
     playerList = []
-    print("test")
     while (districtNum <= numOfDistricts):
         print("District",districtNum)
-        player1 = input("Enter your first volunteer:  ")
-        player2 = input("Enter your second volunteer: ")
-        print(player1, "and", player2, "have been entered into District", districtNum)
+        playerOneName = input("Enter your first volunteer:  ")
+        playerTwoName = input("Enter your second volunteer: ")
+        
+        # Create the players in the district
+        player1 = Player(playerOneName, 69)
+        player2 = Player(playerTwoName, 420)
+
+        # Make them allies
+        player1.setAlly(player2)
+        player2.setAlly(player1)
+
+        print(player1.name, "and", player2.name, "have been entered into District", districtNum)
         print("\n")
         
         # Add the district to the playerList
         playerList.append([player1, player2])
 
-        districtNum += districtNum
+        districtNum += 1
     
     return playerList
 
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, name, health=5):
+        self.name    = name
+        self.health  = health
+        self.allies  = [] # tried passing this in via the constructor, but it got kind of awkward with setting allies in initGame()
+        self.enemies = [] # Players don't have enemies at the start of the game
+    
+    def setHealth(self, health):
+        self.health = health
+        
+    def setAlly(self, ally):
+        self.allies.append(ally)
+    
+    def setEnemy(self, enemy):
+        self.enemies.append(enemy)
 
 if __name__ == "__main__":
     main()
