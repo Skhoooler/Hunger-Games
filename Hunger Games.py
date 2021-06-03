6127d0f0003329f405700691daa681f401367c7d
Skhoooler <dor15004@byui.edu>
2021-06-03 12:14:44 -0600

    Created game loop

--- Hunger Games.py
+++ Hunger Games.py
@@ -4,13 +4,35 @@
 import sys
 
 def main():
-    playerList = initGame(5)
+    playerList, numOfPlayers = initGame(1)
 
-    pass
+    print("###########################################################")
+    print("#                                                         #")
+    print("#              Let the Hunger Games Begin                 #")
+    print("#                                                         #")
+    print("###########################################################")
+    print("\n")
+    dayNum = 1
+    while(numOfPlayers > 1):
+        deadPlayers = nextDay(dayNum)
+        
+        dayNum += 1
+        numOfPlayers -= deadPlayers
+
+
+def nextDay(dayNum):
+    print("###################  Dawn of Day", dayNum," ######################")
+    deadPlayers = 1
+
+    # Stuff here
+
+    return deadPlayers
 
 def initGame(numOfDistricts=12):
-    districtNum = 1
+    districtNum  = 1
+    numOfPlayers = 0
     playerList = []
+
     while (districtNum <= numOfDistricts):
         print("District",districtNum)
         playerOneName = input("Enter your first volunteer:  ")
@@ -19,6 +41,7 @@ def initGame(numOfDistricts=12):
         # Create the players in the district
         player1 = Player(playerOneName, 69)
         player2 = Player(playerTwoName, 420)
+        numOfPlayers += 2
 
         # Make them allies
         player1.setAlly(player2)
@@ -32,7 +55,7 @@ def initGame(numOfDistricts=12):
 
         districtNum += 1
     
-    return playerList
+    return playerList, numOfPlayers
 
 class Player:
     def __init__(self, name, health=5):
@@ -43,7 +66,7 @@ class Player:
     
     def setHealth(self, health):
         self.health = health
-        
+
     def setAlly(self, ally):
         self.allies.append(ally)
     
